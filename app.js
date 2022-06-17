eb0766cda7641360c799063fb8f1cbf612d78849
Dasha <daryagoryacheva94@gmail.com>
2022-06-18 00:32:05 +0300

    last revision version

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 let picArr = ['fa-diamond', 'fa-paper-plane-o', 'fa-anchor', 'fa-bolt', 'fa-cube', "fa fa-bomb", 'fa-leaf', 'fa-bicycle', 'fa-diamond', 'fa-paper-plane-o', 'fa-anchor', 'fa-bolt', 'fa-cube', "fa fa-bomb", 'fa-leaf', 'fa-bicycle'];
 const cardList = document.getElementsByClassName('card');
-let pictureList=document.getElementsByTagName('i');
+let pictureList = document.getElementsByTagName('i');
 const r = document.querySelector('.fa-repeat');
 let m = document.querySelector('.moves');
 let deck = document.querySelector('.deck');
-let picList = document.querySelectorAll('i');
 let starList = document.getElementsByClassName('empty');
 let numMoves = 0;
 let newArr = [];
@@ -21,25 +20,19 @@ function gameFunc() {
 
 /*2nd function shuffle all the cards */
 function newGame() {
-    let newCards = shuffle(picArr).map(function (name) {
-        return '<li class=\'card\'>' +
-            '<i class=\'fa ' + name + '\'></i>' +
-            '</li>';
+    let newCards = shuffle([...cardList]);
+    deck.innerHTML = '';
+    newCards.forEach(element => {
+        deck.appendChild(element);
     });
-    deck.innerHTML = newCards.join('');
 }
 /*3rd function  adds classes 'open' and'show' to open cards. if two open cards have the same class then adds class 'match'*/
 function classesFunc(event) {
     // object user clicked on (li or i):
     let openEl = event.target;
-
-    if (openEl.tagName === 'I') {
-        openEl = openEl.parentElement;
-    }
-
     if (openEl.classList.contains('open') && openEl.classList.contains('show') || openEl.classList.contains('match')) {
         console.log('double tap');
-        return;
+        return;// interrupt function
     }
 
     countMoves();
@@ -47,12 +40,10 @@ function classesFunc(event) {
     newArr.push(openEl);
 
     if (newArr.length > 1) {
-        let first=newArr.pop(); 
-        let second=newArr.pop(); 
-
-        let first_i = first.getElementsByTagName('i')[0];
-        let second_i = second.getElementsByTagName('i')[0];
-
+        let first = newArr.pop();
+        let second = newArr.pop();
+        let first_i = first.firstElementChild;
+        let second_i = second.firstElementChild;
         if (first_i.className !== second_i.className) {
             console.log('no');
             setTimeout(() => {
@@ -62,24 +53,25 @@ function classesFunc(event) {
         }
         else {
             console.log('yep');
-            first.classList.remove('open','show');
+            first.classList.remove('open', 'show');
             first.classList.add('match');
-            second.classList.remove('open','show');
+            second.classList.remove('open', 'show');
             second.classList.add('match');
         }
     }
-    
+
+
     finishGameFunc();
 }
 /* 4rth function is counting moves in Game*/
 function countMoves() {
     numMoves++;
     m.innerHTML = numMoves;
-    if (numMoves === 5) {
+    if (numMoves === 43) {
         console.log('only two stars');
         starList[0].classList.remove('fa-star');
     }
-    else if (numMoves === 8) {
+    else if (numMoves === 55) {
         console.log('only one star');
         starList[1].classList.remove('fa-star');
     }
